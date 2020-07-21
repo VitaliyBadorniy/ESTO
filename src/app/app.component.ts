8b10203281b22fabef3b1b74e038a949ba9e18fb
Badornyi Vitalii <vitalij.badyornyj@mcmed.ua>
2020-07-21 22:19:44 +0300

    git init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-
+import {Component, OnInit} from '@angular/core';
+import * as faker from 'faker/locale/en';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ESTO';
+
 }
